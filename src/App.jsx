3e11270006dd512d95557617bb1e484484927251
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-03-21 03:48:26 +0500

    some design updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,123 @@
-import { useEffect, useState } from "react";
+import { useCallback, useRef, useState } from "react";
+
+const generateRandomPassword = (
+  length,
+  includeSpCharacters,
+  includeNumbers,
+  setPassword
+) => {
+  let characterPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const digits = "0123456789";
+  const spCharacters = "!@#$%^&*()_+-=";
+  let password = "";
+
+  if (includeNumbers) characterPool += digits;
+  if (includeSpCharacters) characterPool += spCharacters;
+
+  for (let i = 0; i < length; i++) {
+    password += characterPool.charAt(
+      Math.floor(Math.random() * characterPool.length)
+    );
+  }
+  setPassword(password);
+  console.log(password);
+};
+
+const copyPasswordToClipboard = (password, passwordRef) => {
+  // console.log(window.navigator.clipboard);
+  passwordRef.current.select();
+  window.navigator.clipboard.writeText(password);
+};
 
 export default function App() {
-  const [from, setFrom] = useState("aud");
-  const [to, setTo] = useState("pkr");
-  const [currencies, setCurrencies] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const [rates, setRates] = useState({});
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [length, setLength] = useState(8);
+  const [spCharAllowed, setSpCharAllowed] = useState(false);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef(null);
 
-  function convert() {
-    console.log(rates);
-    setConvertedAmount(amount * rates[to]);
-  }
+  useCallback(generateRandomPassword);
+  useCallback(copyPasswordToClipboard);
 
-  useEffect(() => convert(), [to, from, rates]);
+  const handleCharacterCheckbox = () => {
+    setSpCharAllowed((prevIsCharacter) => !prevIsCharacter);
+  };
 
-  function swap() {
-    setFrom(to);
-    setTo(from);
-    // console.log(to);
-    // console.log(from);
-  }
-
-  useEffect(() => {
-    (async function fetchCurrencyAPI() {
-      try {
-        const response = await fetch(
-          `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`
-        );
-        const data = await response.json();
-        // console.log(typeof data);
-        // Object.keys(data).forEach((key) => {
-        //   console.log(data[key]);
-        //   setCurrencies((prev) => [...prev, data[key]]);
-        // });
-        // console.log(data[from]);
-        setRates(data[from]);
-        // console.log(Object.keys(data[from]));
-        setCurrencies(Object.keys(data[from]));
-      } catch (error) {
-        console.log("Error: ", error);
-      }
-    })();
-  }, [from, to]);
+  const handleNumberCheckbox = () => {
+    setNumAllowed((prevIsNumber) => !prevIsNumber);
+  };
 
   return (
     <div
       className="bg-cover bg-center h-screen flex flex-col items-center justify-center"
       style={{
         backgroundImage:
-          'url("https://res.cloudinary.com/dm4rvbu7y/image/upload/v1710450157/ReactPractice/wvjxhk0bfnuad2j3lw88.jpg")',
+          'url("https://res.cloudinary.com/dm4rvbu7y/image/upload/v1710973426/ReactPractice/ns0saua800ugvtycuvfc.jpg")',
       }}
     >
-      <h1 className="text-[#E9F1F7] glass my-5 text-3xl font-bold w-4/5 sm:w-3/5 md:w-1/2 lg:w-2/5 xl:w-1/3 2xl:w-1/4 text-center rounded">
-        Currency Converter
-      </h1>
-      <div className="glass w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-1/2 h-1/3 p-3 rounded-lg">
-        <div className="h-1/3 p-2">
-          <div className="flex justify-between items-center mb-5">
-            <p className="text-[#E9F1F7] text-xl">From</p>
-            <p className="text-[#E9F1F7] text-xl">Currency Type</p>
-          </div>
-          <div className="flex justify-between items-center">
-            <input
-              className="appearance-none outline-none px-2 text-lg text-[#E7DFC6] w-1/3 bg-inherit border-b border-solid border-b-[#131B23]"
-              type="number"
-              min={0}
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-            />
-            <select
-              value={from}
-              onChange={(e) => setFrom(e.target.value)}
-              className="w-20 px-1 py-1 border border-solid border-[#E7DFC6] rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#2274A5]"
-            >
-              <option value={from}>{from.toUpperCase()}</option>
-              {currencies.map((currency) => (
-                <option key={currency} value={currency}>
-                  {currency.toUpperCase()}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div className="h-1/3 p-2">
-          <div className="flex justify-between items-center mb-5">
-            <p className="text-[#E9F1F7] text-xl">To</p>
-            <p className="text-[#E9F1F7] text-xl">Currency Type</p>
-          </div>
-          <div className="flex justify-between items-center">
-            <input
-              className="appearance-none outline-none px-2 text-lg text-[#E7DFC6] w-1/3 bg-inherit border-b border-solid border-b-[#131B23]"
-              type="number"                             
-              min={0}
-              value={convertedAmount}
-              readOnly
-            />
-            <select
-              value={to}
-              onChange={(e) => setTo(e.target.value)}
-              className="w-20 px-1 py-1 border border-solid border-[#E7DFC6] rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#2274A5]"
-            >
-              {currencies.map((currency) => (
-                <option key={currency} value={currency}>
-                  {currency.toUpperCase()}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div className="h-1/3 flex justify-center items-center">
+      <h1 className="font-semibold my-5 text-3xl glass px-3 rounded-sm text-[#307473]">Password Generator</h1>
+      <div className="w-8/12 h-40 glass rounded flex flex-col text-[#F9F9F9]">
+        <div className="flex justify-start">
+          <input
+            readOnly
+            value={password}
+            placeholder="Password"
+            ref={passwordRef}
+            className="w-2/3 h-10 my-5 ml-5 outline-none box-border text-lg border border-solid border-[#FFFAFF] p-2 text-[#474350]"
+          ></input>
           <button
-            onClick={convert}
-            className="text-[#E9F1F7] font-medium w-4/6 h-1/2 mr-5 bg-[#2274A5] shadow-2xl outline outline-offset-2 outline-1 outline-[#2274A5] hover:scale-[1.03] hover:outline-none duration-300 active:scale-[0.99]"
+            onClick={() => copyPasswordToClipboard(password, passwordRef)}
+            className="bg-[#F4B942] h-10 w-1/3 my-5 mr-5 cursor-pointer"
           >
-            Convert {from.toUpperCase()} to {to.toUpperCase()}
-          </button>
-          <button
-            onClick={swap}
-            className="text-[#E9F1F7] font-medium w-1/6 h-1/2 ml-5 bg-[#816C61] shadow-2 outline outline-offset-2 outline-1 outline-[#fff] hover:scale-[1.03] hover:outline-none duration-300 active:scale-[0.99]"
-          >
-            Swap
+            Copy
           </button>
         </div>
+        <div className="flex justify-evenly mb-4">
+          <div className="flex items-center">
+            <input
+              className="mx-1"
+              type="range"
+              defaultValue={8}
+              min={8}
+              max={20}
+              onChange={(e) => setLength(e.target.value)}
+            ></input>
+            <span>Length: {length}</span>
+          </div>
+          <div className="flex items-center">
+            <input
+              className="mx-1"
+              type="checkbox"
+              onChange={handleCharacterCheckbox}
+              checked={spCharAllowed}
+              name="Sp. Characters"
+            />
+            <span htmlFor="Sp. Characters">Sp. Characters</span>
+          </div>
+          <div className="flex items-center">
+            <input
+              className="mx-1"
+              type="checkbox"
+              onChange={handleNumberCheckbox}
+              checked={numAllowed}
+              name="Numbers"
+            />
+            <span htmlFor="Numbers">Numbers</span>
+          </div>
+        </div>
+        <button
+          onClick={() =>
+            generateRandomPassword(
+              length,
+              spCharAllowed,
+              numAllowed,
+              setPassword
+            )
+          }
+          className="bg-[#307473] w-1/4 self-center h-8 cursor-pointer"
+        >
+          Generate
+        </button>
       </div>
     </div>
   );
